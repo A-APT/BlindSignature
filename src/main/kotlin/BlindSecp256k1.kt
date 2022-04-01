bfa7efc9ecbeb28aff3a1c3c7ea9bc8c7ed44de2
0JIEUN0 <je300512@gmail.com>
2022-04-01 11:58:16 +0900

    init BlindSecp256k1

---
+++ src/main/kotlin/BlindSecp256k1.kt
@@ -0,0 +1,17 @@
+class BlindSecp256k1 {
+
+    fun newRequestParameters() {}
+
+    fun blind() {}
+
+    fun blindSign() {}
+
+    fun unblind() {}
+
+    fun verify() {}
+
+}
+
+fun main() {
+    println("BlindSecp256k1")
+}
