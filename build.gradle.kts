af49b38e0364f10bd7452adb8b85167087231611
0JIEUN0 <je300512@gmail.com>
2022-04-05 15:34:19 +0900

    Try to do blind-sig using multiplier of ECCurve

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.64")
 }
 
 tasks.test {
--- src/main/kotlin/BlindSecp256k1.kt
+++ src/main/kotlin/BlindSecp256k1.kt
@@ -1,31 +1,46 @@
+import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util
+import org.bouncycastle.math.ec.ECCurve
+import org.bouncycastle.math.ec.ECMultiplier
+import org.bouncycastle.math.ec.ECPoint
 import java.math.BigInteger
 import java.security.SecureRandom
+import java.security.spec.ECFieldFp
 
-data class KeyPair(val publicKey: Point, val privateKey: BigInteger)
-data class BlindedData(val a: BigInteger, val b: BigInteger, val R: Point, val blindM: BigInteger)
+data class KeyPair(val publicKey: ECPoint, val privateKey: BigInteger)
+data class BlindedData(val a: BigInteger, val b: BigInteger, val R: ECPoint, val blindM: BigInteger)
 
 class BlindSecp256k1 {
 
     val curve = EllipticCurve()
     val random = SecureRandom()
+    var multiplier: ECMultiplier
+    var G: ECPoint
+
+    init {
+        val c = java.security.spec.EllipticCurve(ECFieldFp(curve.P), curve.a, curve.b)
+        val ecCurve: ECCurve = EC5Util.convertCurve(c)
+        multiplier = ecCurve.multiplier
+        G = ecCurve.createPoint(curve.Gx, curve.Gy)
+    }
 
     fun generateKeyPair(): KeyPair {
         val private = BigInteger.valueOf(random.nextLong()) // BigInteger(curve.N.bitLength(), random).mod(curve.N)
-        val public = curve.mul(private)
+        val public = multiplier.multiply(G, private) // curve.mul(private)
         return KeyPair(public, private)
     }
 
-    fun newRequestParameters(): Pair<BigInteger, Point> {
+    fun newRequestParameters(): Pair<BigInteger, ECPoint> {
         val k = BigInteger.valueOf(random.nextLong())
-        val R_ = curve.mul(k)
+        val R_ = multiplier.multiply(G, k) // curve.mul(k)
         return Pair(k, R_) // R' = kG
     }
 
-    fun blind(R_: Point, m: BigInteger): BlindedData {
+    fun blind(R_: ECPoint, m: BigInteger): BlindedData {
         val a = BigInteger.valueOf(random.nextLong())
         val b = BigInteger.valueOf(random.nextLong())
-        val R = curve.add(curve.mul(a, R_), curve.mul(b)) // R=aR'+bG
-        val blindM: BigInteger = (R.x * m) / a // TODO hash m
+        val R = multiplier.multiply(R_, a).add(multiplier.multiply(G, b)) // curve.add(curve.mul(a, R_), curve.mul(b)) // R=aR'+bG
+        val a_1 = a.modInverse(curve.N)
+        val blindM: BigInteger = (a_1 * R.normalize().xCoord.toBigInteger() * m).mod(curve.N) // TODO hash m
         return BlindedData(a, b, R, blindM)
     }
 
@@ -37,10 +52,11 @@ class BlindSecp256k1 {
         return (a * blindSig + b).mod(curve.N) // s = as' + b
     }
 
-    fun verify(sig: BigInteger, R: Point, m: BigInteger): Boolean {
-        val left = curve.mul(sig) // sG
-        val right = curve.add(R, curve.mul(R.x * m)) // TODO h(m) // R + xRh(m)G
-        return left == right
+    fun verify(sig: BigInteger, R: ECPoint, m: BigInteger): Boolean {
+        val left = multiplier.multiply(G, sig) //curve.mul(sig) // sG
+        val right = R.add(multiplier.multiply(G, R.normalize().xCoord.toBigInteger() * m)) // TODO h(m) // R + xRh(m)G
+        return left.normalize().xCoord.toBigInteger() == right.normalize().xCoord.toBigInteger()
+                && left.normalize().yCoord.toBigInteger() == right.normalize().yCoord.toBigInteger()
     }
 
 }
@@ -59,5 +75,6 @@ fun main() {
     val sig: BigInteger             = blind.unblind(blindedData.a, blindedData.b, blindSig)
 
     val result: Boolean             = blind.verify(sig, blindedData.R, m)
+    println(result)
     assert(result)
 }
