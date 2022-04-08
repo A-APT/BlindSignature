5e9816ef3405f35c407cb5bb0e0d486ec95fc6ea
0JIEUN0 <je300512@gmail.com>
2022-04-08 20:56:55 +0900

    refactor: package

--- src/test/kotlin/com/aapt/BlindSecp256k1Test.kt
+++ src/test/kotlin/com/aapt/BlindSecp256k1Test.kt
@@ -1,3 +1,8 @@
+package com.aapt
+
+import BlindSecp256k1
+import BlindedData
+import KeyPair
 import org.junit.jupiter.api.Test
 import java.math.BigInteger
 import kotlin.test.assertEquals
@@ -8,10 +13,10 @@ class BlindSecp256k1Test {
     fun is_blindsecp256k1_works_well() {
         val blind = BlindSecp256k1()
         val m: ByteArray = "test".encodeToByteArray() // UTF-8
-        val keyPair: KeyPair            = blind.generateKeyPair()
+        val keyPair: KeyPair = blind.generateKeyPair()
         val (k, R_)                     = blind.newRequestParameters()
 
-        val blindedData: BlindedData    = blind.blind(R_, m)
+        val blindedData: BlindedData = blind.blind(R_, m)
         val blindSig: BigInteger = blind.blindSign(keyPair.privateKey, k, blindedData.blindM)
         val sig: BigInteger = blind.unblind(blindedData.a, blindedData.b, blindSig)
 
